14fcc1d7dfbbf24b838fbe38b8c522282bc168e4
Prachi <prachipramodkumar15@gmail.com>
2024-07-01 16:02:35 +0530

    Rollback

--- Backend/routes/doctorRoutes.js
+++ Backend/routes/doctorRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const doctorController = require("../controllers/DoctorController");
 
 const router = express.Router();
-//we need to seperate route file from controller file later
+
 router.use("/doctor", doctorController);
 
 module.exports = router;
